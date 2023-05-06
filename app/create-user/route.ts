138154f407540ada4938b6afc5a671501e1b07b6
Max Holland <max@livepeer.org>
2023-05-06 15:45:24 +0100

    room joining

---
+++ app/create-user/route.ts
@@ -0,0 +1,22 @@
+export async function POST(req) {
+    const post = await req.json();
+    console.log(post)
+    const response = await fetch(`https://livepeer.monster/api/room/${post.roomID}/user`, {
+        method: 'POST',
+        headers: {
+            Authorization: `Bearer ${process.env.LP_AUTH}`,
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            name: post.name
+        }),
+    });
+
+    const data = await response.json();
+
+    console.log(data)
+
+    return new Response(JSON.stringify(data), {
+        status: 200
+    })
+}
