2b24c0c7fb75276a4eddc135ffae4a0ddac913e6
Max Holland <max@livepeer.org>
2023-05-11 16:45:27 +0100

    set favicon

--- app/layout.js
+++ app/layout.js
@@ -4,14 +4,18 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Rooms',
+  title: 'Livepeer Rooms',
   description: '',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+        <head>
+          <link rel="icon" type="image/png" href="/favicon.png" />
+        </head>
+
+        <body className={inter.className}>{children}</body>
     </html>
   )
 }
